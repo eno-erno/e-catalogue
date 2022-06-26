0eab3cc39751bb25723ddf247ffcad141444b575
eno-erno <eno.erno@gmail.com>
2022-06-26 14:15:02 +0700

    sip jos tenan

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-   echo "Sip";
+   echo "Sip jos tenan";
     return $request->user();
 });
